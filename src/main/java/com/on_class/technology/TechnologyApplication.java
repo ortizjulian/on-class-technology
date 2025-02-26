ab255a17b51db03bc42058953edc6c7f7e17abe8
Julian Ortiz <julian.ortiz@pragma.com.co>
2025-02-26 10:59:30 -0500

    spring intializr

---
+++ src/main/java/com/on_class/technology/TechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.on_class.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyApplication.class, args);
+	}
+
+}
